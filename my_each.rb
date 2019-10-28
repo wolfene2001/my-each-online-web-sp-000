52ccfabba71296fe10a35c8509496681861386b5
wolfene2001 <wolfene2001@yahoo.com>
2019-10-28 11:24:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each (array)
+  i = 0
+  while i < array.length
+    yield (array[i])
+    i = i + 1
+  end
+  array# put argument(s) here
   # code here
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  return i
 end
